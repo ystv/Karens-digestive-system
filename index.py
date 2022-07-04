693e3611087f84a4813bead3067b07750dd2fd67
1950sGraphics <xdthediamondcake@gmail.com>
2022-07-04 03:47:52 +0100

    Bug Fixes

--- index.py
+++ index.py
@@ -6,17 +6,6 @@ statusWindow = __import__("status-windows")
 karensBrain = __import__("karens-brain")
 loop = True
 
-# Detect Removeable Media
-
-def detect_media():
-    global loop
-    while loop == True:
-        time.sleep(1)
-        if os.system("ls /media | grep sd >/dev/null 2>&1") == 0:
-            # add next window here
-            karensBrain.instruction.set("SD Found")
-detect_media_background = multiprocessing.Process(target=detect_media)
-
 # Checking for required files
 
 def find(name, path):
@@ -30,7 +19,6 @@ if (
     find("Karen Config.txt",os.getcwd()) == True,
     find("Karens Brain.py",os.getcwd()) == True
 ):
-    detect_media_background.start()
     karensBrain.main_window()
 else:
     statusWindow.error_window("Required Files Missing")
--- karens-brain.py
+++ karens-brain.py
@@ -2,9 +2,21 @@
 
 # Imports ---------
 import tkinter as tk
-statusWindow = __import__("status-windows") 
+import time
+import os
+statusWindow = __import__("status-windows")
+
 def enter_pressed(e):
         statusWindow.error_window("Function Incomplete")
+
+# Detect Removeable Media ------
+def detect_media():
+    global loop
+    while loop == True:
+        time.sleep(1)
+        if os.system("ls /media | grep sd >/dev/null 2>&1") != "":
+            # add next window here
+            instruction.set("SD Found")
 
 # GUI ----------
 def main_window():
@@ -25,6 +37,7 @@ def main_window():
         text = tk.Label(GUI,text="Karen's Digestive System (v "+version+") by Jamie",height=10,font=50,bg='#597685',fg='white')
         text.pack(side="bottom")
         text = tk.Label(GUI,text="This is a Beta test, please report any bugs in #computing on Slack\nPlease confirm your footage is on pending edits before putting the SD card away!",height=5,font=("TkSubHeadingFont",20),bg='#597685',fg='white')
-        text.pack(side="bottom")
+        text.pack(side="bottom")#
         GUI.bind('<Return>',enter_pressed)
+        GUI.after(0, detect_media)
         GUI.mainloop()
