710b490daf59fd24652df8f879a898987803261d
1950sGraphics <xdthediamondcake@gmail.com>
2023-07-08 17:23:52 +0100

    Removed local testing files from repo

--- test.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-filedialog.askdirectory(initialdir="/",
-                            title="Select a Production Folder")
-
-                            #Grep each word?
-                            # Is there a way to show directories live as you search
